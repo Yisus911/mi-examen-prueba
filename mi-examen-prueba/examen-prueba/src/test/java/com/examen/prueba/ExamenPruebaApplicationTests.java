626c129ec6095b5f1f3066b55fc85232baa6d28a
Yisus911 <103014379+Yisus911@users.noreply.github.com>
2025-03-21 15:31:35 -0600

    Add files via upload

--- mi-examen-prueba/examen-prueba/src/test/java/com/examen/prueba/ExamenPruebaApplicationTests.java
+++ mi-examen-prueba/examen-prueba/src/test/java/com/examen/prueba/ExamenPruebaApplicationTests.java
@@ -18,117 +18,117 @@ import java.util.Optional;
 @SpringBootTest
 class ExamenPruebaApplicationTests {
 
-//	@Mock
-//	private TelefonoRepository repository;
-//	@InjectMocks
-//	private TelefonoService service;
-//
-//	@Test
-//	void getTelefonoByImei() throws Exception {
-//		//given
-//		Telefono tel1 = new Telefono("123abc56789",
-//				"Samsung S23 Ultra",
-//				"Samsung",
-//				"Galaxy S23 Ultra",
-//				"S23 Ultra",
-//				new Date(),
-//				1234567890L,
-//				4422648297L,
-//				"jesuspc_905@hotmail.com",
-//				true);
-//		Telefono telefono = Telefono.builder()
-//				.id("123abc56789")
-//				.nombre("Samsung S23 Ultra")
-//				.marca("Samsung")
-//				.modelo("Galaxy S23 Ultra")
-//				.nombreCorto("S23 Ultra")
-//				.fechaCreacion(new Date())
-//				.imei(1234567890L)
-//				.numeroCelular(4422648297L)
-//				.emailSoporte("jesuspc_905@hotmail.com")
-//				.isIOS(true)
-//				.build();
-//		//When
-//		given(repository.findByImei(1111L))
-//				.willReturn(Optional.of(telefono));
-//		var  tel = service.getTelefonoByImei(1111L);
-//		//Then
-//
-//		assertThat(tel).isNotNull();
-//	}
-//
-//	@Test
-//	void getTelefonoPorId() throws Exception {
-//		//given
-//		Telefono tel1 = new Telefono("123abc56789",
-//				"Samsung S23 Ultra",
-//				"Samsung",
-//				"Galaxy S23 Ultra",
-//				"S23 Ultra",
-//				new Date(),
-//				1234567890L,
-//				4422648297L,
-//				"jesuspc_905@hotmail.com",
-//				true);
-//		Telefono telefono = Telefono.builder()
-//				.id("123abc56789")
-//				.nombre("Samsung S23 Ultra")
-//				.marca("Samsung")
-//				.modelo("Galaxy S23 Ultra")
-//				.nombreCorto("S23 Ultra")
-//				.fechaCreacion(new Date())
-//				.imei(1234567890L)
-//				.numeroCelular(4422648297L)
-//				.emailSoporte("jesuspc_905@hotmail.com")
-//				.isIOS(true)
-//				.build();
-//		//When
-//		given(repository.findById("123abc"))
-//				.willReturn(Optional.of(telefono));
-//		var  tel = service.getTelefonoPorId("123abc");
-//		//Then
-//
-//		assertThat(tel).isNotNull();
-//	}
-//
-//	@Test
-//	void getTodos() throws Exception {
-//		//given
-//		Pageable pagina = PageRequest.of(1, 3);
-//		Telefono tel1 = new Telefono("123abc56789",
-//				"Samsung S23 Ultra",
-//				"Samsung",
-//				"Galaxy S23 Ultra",
-//				"S23 Ultra",
-//				new Date(),
-//				1234567890L,
-//				4422648297L,
-//				"jesuspc_905@hotmail.com",
-//				true);
-//		Telefono telefono = Telefono.builder()
-//				.id("123abc56789")
-//				.nombre("Samsung S23 Ultra")
-//				.marca("Samsung")
-//				.modelo("Galaxy S23 Ultra")
-//				.nombreCorto("S23 Ultra")
-//				.fechaCreacion(new Date())
-//				.imei(1234567890L)
-//				.numeroCelular(4422648297L)
-//				.emailSoporte("jesuspc_905@hotmail.com")
-//				.isIOS(true)
-//				.build();
-//		//When
-//		given(repository.findAll())
-//				.willReturn(List.of(telefono, tel1));
-//		var telList = service.getAll(pagina);
-//		//Then
-//
-//		assertThat(telList).isNotNull();
-//		assertThat(telList.getTotalPages()).isGreaterThan(0);
-//	}
-//
-//	@Test
-//	void contextLoads() {
-//	}
+	@Mock
+	private TelefonoRepository repository;
+	@InjectMocks
+	private TelefonoService service;
+
+	@Test
+	void getTelefonoByImei() throws Exception {
+		//given
+		Telefono tel1 = new Telefono("123abc56789",
+				"Samsung S23 Ultra",
+				"Samsung",
+				"Galaxy S23 Ultra",
+				"S23 Ultra",
+				new Date(),
+				1234567890L,
+				4422648297L,
+				"jesuspc_905@hotmail.com",
+				true);
+		Telefono telefono = Telefono.builder()
+				.id("123abc56789")
+				.nombre("Samsung S23 Ultra")
+				.marca("Samsung")
+				.modelo("Galaxy S23 Ultra")
+				.nombreCorto("S23 Ultra")
+				.fechaCreacion(new Date())
+				.imei(1234567890L)
+				.numeroCelular(4422648297L)
+				.emailSoporte("jesuspc_905@hotmail.com")
+				.isIOS(true)
+				.build();
+		//When
+		given(repository.findByImei(1111L))
+				.willReturn(Optional.of(telefono));
+		var  tel = service.getTelefonoByImei(1111L);
+		//Then
+
+		assertThat(tel).isNotNull();
+	}
+
+	@Test
+	void getTelefonoPorId() throws Exception {
+		//given
+		Telefono tel1 = new Telefono("123abc56789",
+				"Samsung S23 Ultra",
+				"Samsung",
+				"Galaxy S23 Ultra",
+				"S23 Ultra",
+				new Date(),
+				1234567890L,
+				4422648297L,
+				"jesuspc_905@hotmail.com",
+				true);
+		Telefono telefono = Telefono.builder()
+				.id("123abc56789")
+				.nombre("Samsung S23 Ultra")
+				.marca("Samsung")
+				.modelo("Galaxy S23 Ultra")
+				.nombreCorto("S23 Ultra")
+				.fechaCreacion(new Date())
+				.imei(1234567890L)
+				.numeroCelular(4422648297L)
+				.emailSoporte("jesuspc_905@hotmail.com")
+				.isIOS(true)
+				.build();
+		//When
+		given(repository.findById("123abc"))
+				.willReturn(Optional.of(telefono));
+		var  tel = service.getTelefonoPorId("123abc");
+		//Then
+
+		assertThat(tel).isNotNull();
+	}
+
+	@Test
+	void getTodos() throws Exception {
+		//given
+		Pageable pagina = PageRequest.of(1, 3);
+		Telefono tel1 = new Telefono("123abc56789",
+				"Samsung S23 Ultra",
+				"Samsung",
+				"Galaxy S23 Ultra",
+				"S23 Ultra",
+				new Date(),
+				1234567890L,
+				4422648297L,
+				"jesuspc_905@hotmail.com",
+				true);
+		Telefono telefono = Telefono.builder()
+				.id("123abc56789")
+				.nombre("Samsung S23 Ultra")
+				.marca("Samsung")
+				.modelo("Galaxy S23 Ultra")
+				.nombreCorto("S23 Ultra")
+				.fechaCreacion(new Date())
+				.imei(1234567890L)
+				.numeroCelular(4422648297L)
+				.emailSoporte("jesuspc_905@hotmail.com")
+				.isIOS(true)
+				.build();
+		//When
+		given(repository.findAll())
+				.willReturn(List.of(telefono, tel1));
+		var telList = service.getAll(pagina);
+		//Then
+
+		assertThat(telList).isNotNull();
+		assertThat(telList.getTotalPages()).isGreaterThan(0);
+	}
+
+	@Test
+	void contextLoads() {
+	}
 
 }
